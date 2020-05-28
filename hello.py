f23980197332ddedf673e7758ba848a203c09477
DRAGON79000 <50417333+DRAGON79000@users.noreply.github.com>
2020-05-28 12:31:11 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-class operation:
+#I create a class so that the code is a lot easier to read
+class operation: 
     def __init__(self,x,y):
         self.x = int(x)
         self.y = int(y)
@@ -25,7 +26,7 @@ class operation:
             return divide()
         else:
             return "sorry invalid operator."
-
+# I create a function that basically finds the answer with the class above
 def operational3(my_text):
     my_text = my_text.split()
     while len(my_text) != 1:
@@ -37,9 +38,10 @@ def operational3(my_text):
         my_text.insert(0,answer)
     answer_final = my_text[0]
     return answer
-
+# I prompt the user for an equation
 print("Hello. This is a calculator. Please type in your equation. Don't type in the equal symbol.please give a space between each number or operator.Also this calculator is useful for making small calulations only.")
 my_text = input()
+# I call the function that I created
 print(operational3(my_text))
 running = True
 while running:
